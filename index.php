ca5c9a02787e7838ed75e2690a7b336082b28b0f
hadiansyaah <hadiansyah0015@gmail.com>
2019-09-23 19:15:46 -0700

    free

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>latihan bootstrap</title>
+	<!--pemanggilan css --->
+	<link rel="stylesheet" href="/assets/css/bootstrap.mon.css">
+	<!-- akhir pemanggilan css-->
+</head>
+<body>
+
+<!-- header -->
+<!-- End header -->
+
+<!-- content -->
+<div class="container">
+	<div class="row">
+		<div class="col-md 12" style="background: violet">
+			<h1> 12 coloum</h1>
+		</div>
+	</div>
+<div class="row">
+       <div class="col-md 6" style="background: red">
+       	<h1> 6 coloum</h1>
+       </div>	
+<div class="row">	
+    <div class="col-md 6 " style="background: purple">
+       	<h1>6 coloum</h1>
+       </div>
+	</div>
+</div>
+<!-- End content -->
+
+<!-- footer -->
+<!-- end footer -->
+
+<!-- pemanggilan js -->
+<script src="/assets/js/bootstrap.budle.js"></script>
+
+<script src="/assets/js/bootstrap.budle.min.css"></script>
+<!-- akhir pemanggilan css -->
+
+</body>
+</html>
